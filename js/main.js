fdb8f3f41fe6ffce43b8d276e57c3adaf95a05b9
Galyna Savchenko <galyna.chui@gmail.com>
2019-02-10 20:58:36 +0200

    Reviewer comments fixed

--- js/main.js
+++ js/main.js
@@ -171,10 +171,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'Restaurant image';
+  image.alt = `Image for ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('rest-static-v2').then(function(cache) {
+    caches.open('rest-static-v5').then(function(cache) {
       return cache.addAll(
         [
+          '/sw.js',
           '/css/styles.css',
           '/js/dbhelper.js',
           '/js/main.js',
           '/js/restaurant_info.js',
           '/index.html',
+          '/',
           '/restaurant.html',
           'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
           'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
@@ -19,9 +21,12 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
+    // Try the cache
     caches.match(event.request).then(function(response) {
-      console.log('Returning cache');
+      // Fall back to network
       return response || fetch(event.request);
+    }).catch(function() {
+      console.log('Error', event.request);
     })
   );
 });
